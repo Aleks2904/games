23d3da2281917d59b2a29f6c633429a98432d5f7
Aleks2904 <Garmatin171@yandex.ru>
2020-07-28 11:45:24 +0700

    header start

---
+++ app/js/main.js
@@ -0,0 +1,10 @@
+!function(e){var r={};function t(_){if(r[_])return r[_].exports;var n=r[_]={i:_,l:!1,exports:{}};return e[_].call(n.exports,n,n.exports,t),n.l=!0,n.exports}t.m=e,t.c=r,t.d=function(e,r,_){t.o(e,r)||Object.defineProperty(e,r,{enumerable:!0,get:_})},t.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},t.t=function(e,r){if(1&r&&(e=t(e)),8&r)return e;if(4&r&&"object"==typeof e&&e&&e.__esModule)return e;var _=Object.create(null);if(t.r(_),Object.defineProperty(_,"default",{enumerable:!0,value:e}),2&r&&"string"!=typeof e)for(var n in e)t.d(_,n,function(r){return e[r]}.bind(null,n));return _},t.n=function(e){var r=e&&e.__esModule?function(){return e.default}:function(){return e};return t.d(r,"a",r),r},t.o=function(e,r){return Object.prototype.hasOwnProperty.call(e,r)},t.p="",t(t.s="./src/js/main.js")}({"./src/js/main.js":
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+/*! no exports provided */function(module,__webpack_exports__,__webpack_require__){"use strict";eval('__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _test1__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./test1 */ "./src/js/test1.js");\n\nObject(_test1__WEBPACK_IMPORTED_MODULE_0__["cs"])();\n\n//# sourceURL=webpack:///./src/js/main.js?')},"./src/js/test1.js":
+/*!*************************!*\
+  !*** ./src/js/test1.js ***!
+  \*************************/
+/*! exports provided: cs */function(module,__webpack_exports__,__webpack_require__){"use strict";eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"cs\", function() { return cs; });\nfunction cs() {\n  console.log('123');\n}\n\n//# sourceURL=webpack:///./src/js/test1.js?")}});
+//# sourceMappingURL=main.js.map
